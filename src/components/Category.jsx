0aca53eef9be0699c414345858188e5692920c07
saijaris <saija.ristolainen@gmail.com>
2022-12-15 20:48:30 +0200

    Add link to icons

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,8 +4,8 @@ import './category.css'
 const Category = props => {
     const { categoryName, icon, trnro } = props;
     return <div className='category'>
-        {icon}
-        <a href={'/products/' + trnro}>{categoryName}</a>
+        <a href={'/products/' + trnro}>{icon}
+            {categoryName}</a>
     </div>
 }
 
