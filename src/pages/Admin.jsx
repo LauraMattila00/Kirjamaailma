0930ca0e3ba3ebbf4a14ee83899cc81408e04bfc
saijaris <saija.ristolainen@gmail.com>
2022-12-21 15:45:17 +0200

    Fix admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -10,6 +10,8 @@ function Admin({ url }) {
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
     const submitForm = (event) => {
+        if (value === "")
+            return;
         event.preventDefault();
         setSuccess(false);
         setError(false);
@@ -55,7 +57,7 @@ function Admin({ url }) {
                     id="input-trnimi"
                     onChange={handleChange}
                 />
-                <Button className="button" type="submit">Lisää</Button>
+                <Button className="button" type="submit" disabled={!value}>Lisää</Button>
             </Form>
             <Alert show={success} variant={"info"}>
                 Tuoteryhmän lisääminen onnistui!
@@ -71,3 +73,79 @@ function Admin({ url }) {
 
 
 export default Admin
+
+
+
+
+/*import axios from 'axios';
+import React, { useState } from 'react'
+import { Alert, Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import "./Admin.css";
+
+function Admin({ url }) {
+    const [value, setValue] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const submitForm = (event) => {
+        event.preventDefault();
+        setSuccess(false);
+        setError(false);
+        const json = JSON.stringify({
+            name: value
+        })
+        axios.post(
+            url + 'products/addcategory.php',
+            json,
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+                setSuccess(true);
+                setTimeout(() => {
+                    setSuccess(false)
+                }, 2000);
+            })
+            .catch(function (error) {
+                setError(true);
+                setTimeout(() => {
+                    setError(false)
+                }, 2000);
+
+                console.log(error);
+            });
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        setValue(event.target.value);
+    }
+
+    return (
+        <div id="form-container" className='form-container'>
+            <Form className="form" onSubmit={submitForm}>
+                <Form.Label htmlFor="input-trnimi">Syötä lisättävä tuoteryhmä</Form.Label>
+                <Form.Control
+                    type="text"
+                    id="input-trnimi"
+                    onChange={handleChange}
+                />
+                <Button className="button" type="submit">Lisää</Button>
+            </Form>
+            <Alert className="alert" show={success} variant={"info"}>
+                Tuoteryhmän lisääminen onnistui!
+            </Alert>
+            <Alert id="alert" className="alert" show={error} variant={"danger"}>
+                Tuoteryhmän lisääminen epäonnistui!
+            </Alert>
+
+        </div>
+    )
+}
+
+
+
+export default Admin*/
