f8ae324cec519038a7a3a9ebab793f198f378634
saijaris <saija.ristolainen@gmail.com>
2022-12-21 15:03:16 +0200

    Add admin route

--- src/App.jsx
+++ src/App.jsx
@@ -22,48 +22,48 @@ function App() {
   const [cart, setCart] = useState([]);
   const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        axios.get(URL + 'products/getcategories.php')
-            .then((response) => {
-                const json = response.data;
-                setCategories(json);
-                console.log(json);
-            }).catch(error => {
-                alert(error.response === undefined ? error : error.response.data.error);
-            })
-    }, [])
+  useEffect(() => {
+    axios.get(URL + 'products/getcategories.php')
+      .then((response) => {
+        const json = response.data;
+        setCategories(json);
+        console.log(json);
+      }).catch(error => {
+        alert(error.response === undefined ? error : error.response.data.error);
+      })
+  }, [])
 
   useEffect(() => {
     if ('cart' in localStorage) {
       setCart(JSON.parse(localStorage.getItem('cart')));
     }
   }, [])
-  
+
 
   function addToCart(product) {
     if (cart.some(item => item.tuotenro === product.tuotenro)) {
-      const existingProduct = cart.filter(item => item.tuotenro ===product.tuotenro);
-      updateAmount(parseInt(existingProduct[0].amount) + 1,product);
+      const existingProduct = cart.filter(item => item.tuotenro === product.tuotenro);
+      updateAmount(parseInt(existingProduct[0].amount) + 1, product);
     } else {
-    product['amount'] = 1;
-    const newCart = [...cart, product];
-    setCart(newCart);
-    localStorage.setItem('cart', JSON.stringify(newCart));
+      product['amount'] = 1;
+      const newCart = [...cart, product];
+      setCart(newCart);
+      localStorage.setItem('cart', JSON.stringify(newCart));
     }
   }
 
   function removeFromCart(product) {
     const itemsWithoutRemoved = cart.filter(item => item.tuotenro !== product.tuotenro);
     setCart(itemsWithoutRemoved);
-    localStorage.setItem('cart',JSON.stringify(itemsWithoutRemoved));
+    localStorage.setItem('cart', JSON.stringify(itemsWithoutRemoved));
   }
 
-  function updateAmount(amount,product) {
+  function updateAmount(amount, product) {
     product.amount = amount;
     const index = cart.findIndex((item => item.tuotenro === product.tuotenro));
-    const modifiedCart = Object.assign([...cart],{[index]: product});
+    const modifiedCart = Object.assign([...cart], { [index]: product });
     setCart(modifiedCart);
-    localStorage.setItem('cart',JSON.stringify(modifiedCart));
+    localStorage.setItem('cart', JSON.stringify(modifiedCart));
   }
 
   return (
@@ -72,15 +72,15 @@ function App() {
         <Header />
         <Navbar cart={cart} categories={categories} />
         <Routes>
-          <Route path='/' element={<Home categories={categories}/>} />
+          <Route path='/' element={<Home categories={categories} />} />
           <Route path='/products/:categoryId' element={<Products url={URL} addToCart={addToCart} />} />
           <Route path='/search/:searchPhrase' element={<Products url={URL} addToCart={addToCart} />} />
-          <Route path='/tuotesivu/:productId' element={<Tuotesivu url={URL} addToCart={addToCart}/>} />
-          <Route path='/order' element={<Order url={URL} cart={cart} removeFromCart={removeFromCart} updateAmount={updateAmount}/>} />
-          <Route path='/admin' element={<Admin />} />
-          <Route path='*' element={<NotFound />} />  
-          <Route path='/login' element={<Login url={URL}/>} />
-          <Route path='/signup' element={<Signup url={URL}/>} />
+          <Route path='/tuotesivu/:productId' element={<Tuotesivu url={URL} addToCart={addToCart} />} />
+          <Route path='/order' element={<Order url={URL} cart={cart} removeFromCart={removeFromCart} updateAmount={updateAmount} />} />
+          <Route path='/admin' element={<Admin url={URL} />} />
+          <Route path='*' element={<NotFound />} />
+          <Route path='/login' element={<Login url={URL} />} />
+          <Route path='/signup' element={<Signup url={URL} />} />
         </Routes>
         <Footer />
 
